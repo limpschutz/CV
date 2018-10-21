9eb11b58f93d0caa0e90543fa8144bc677effca9
Linus Petersson <44273912+limpschutz@users.noreply.github.com>
2018-10-21 18:21:59 +0200

    Updates

--- precache-manifest.bd5f2b2541a3c8340dc7548b67cbfda5.js
+++ precache-manifest.bd5f2b2541a3c8340dc7548b67cbfda5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/CV/static/js/runtime~main.7f88df48.js"
   },
   {
-    "revision": "e910b88b7e51c86b1b5c",
-    "url": "/CV/static/js/main.e910b88b.chunk.js"
+    "revision": "b66deee7c3bdf1c34708",
+    "url": "/CV/static/js/main.b66deee7.chunk.js"
   },
   {
     "revision": "1e4473fe3ed90bb9dcd2",
     "url": "/CV/static/js/1.1e4473fe.chunk.js"
   },
   {
-    "revision": "e910b88b7e51c86b1b5c",
+    "revision": "b66deee7c3bdf1c34708",
     "url": "/CV/static/css/main.16080e59.chunk.css"
   },
   {
-    "revision": "9771b609d9bd144f8149f9bcd6642d4d",
+    "revision": "4a5189786b58e797b99527f0fe12b46d",
     "url": "/CV/index.html"
   }
 ];
